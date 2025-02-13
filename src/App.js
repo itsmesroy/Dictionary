8c0254051bd9f4f846704d551539db83fb91a20a
itsmesroy <itsmevicky78@gmail.com>
2025-02-13 11:11:14 +0530

    done changes

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Dictionary App</h1>
+      <div>
+
+        </div>
+    </div>
+  );
+}
+
+export default App;
